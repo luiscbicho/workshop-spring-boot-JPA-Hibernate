bbf68d89e88efb52a0cca5718746d64e7bc15563
Luis Bicho <luisbicho@proton.me>
2024-03-14 17:09:03 +0100

    Project created

---
+++ src/main/java/com/luisbicho/workshopspringboot_jpa_hibernate/WorkshopSpringBootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.luisbicho.workshopspringboot_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBootJpaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopSpringBootJpaHibernateApplication.class, args);
+    }
+
+}
